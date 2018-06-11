aab95381b0ba5224c19a02ba8a8ad4e553ecbf86
SuPoPoo <supopo921@gmail.com>
2018-06-11 20:21:57 +0630

    addeds Procfile,hello and requirements

--- hello.py
+++ hello.py
@@ -3,10 +3,10 @@ from flask import Flask
 app=Flask(_name_)
 
 @app.route("/")
-def.hello():
+def hello():
     return"Hello World"
 
 @app.route("/tiide")
-def.tiide():
+def tiide():
     return"Welcome to TIIDE World"
 
